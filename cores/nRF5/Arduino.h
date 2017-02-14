99e0b384e85bac4eab236d469047cc37a137fa10
Kevin Townsend <microbuilder@users.noreply.github.com>
2017-02-14 03:57:30 +0100

    Revert NRF_GPIO update

--- cores/nRF5/Arduino.h
+++ cores/nRF5/Arduino.h
@@ -101,12 +101,12 @@ uint32_t setLoopStacksize(void);
 
 #define bit(b) (1UL << (b))
 
-#define digitalPinToPort(P)        ( &(NRF_GPIO) )
+#define digitalPinToPort(P)        ( &(NRF_GPIO[P]) )
 #define digitalPinToBitMask(P)     ( 1 << g_ADigitalPinMap[P] )
 //#define analogInPinToBit(P)        ( )
-#define portOutputRegister(port)   ( &(port->OUT) )
-#define portInputRegister(port)    ( &(port->IN) )
-#define portModeRegister(port)     ( &(port->DIRSET) )
+#define portOutputRegister(port)   ( &(NRF_GPIO->OUT) )
+#define portInputRegister(port)    ( &(NRF_GPIO->IN) )
+#define portModeRegister(port)     ( &(NRF_GPIO->DIRSET) )
 #define digitalPinHasPWM(P)        ( true )
 
 /*
