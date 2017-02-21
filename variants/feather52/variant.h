0ad37e8bb2d0969ec991c9e93ea8763863101e81
hathach <thach@tinyusb.org>
2017-02-21 14:54:39 +0700

    clean up

--- variants/feather52/variant.h
+++ variants/feather52/variant.h
@@ -14,8 +14,8 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef _VARIANT_NRF52_DK_
-#define _VARIANT_NRF52_DK_
+#ifndef _VARIANT_FEATHER52_
+#define _VARIANT_FEATHER52_
 
 /** Master clock frequency */
 #define VARIANT_MCK       (64000000ul)
