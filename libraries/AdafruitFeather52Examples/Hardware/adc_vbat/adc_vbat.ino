c75a715b53d707cf1831c9a1be740679e1b31a37
microbuilder <contact@microbuilder.eu>
2017-03-01 18:49:27 +0100

    Added LIPO VBAT conversion

--- libraries/AdafruitFeather52Examples/Hardware/adc_vbat/adc_vbat.ino
+++ libraries/AdafruitFeather52Examples/Hardware/adc_vbat/adc_vbat.ino
@@ -3,8 +3,8 @@
 #define VBAT_DIVIDER      (0.71275837F)   // 2M + 0.806M voltage divider on VBAT = (2M / (0.806M + 2M))
 #define VBAT_DIVIDER_COMP (1.403F)        // Compensation factor for the VBAT divider
 
-float readVBAT(void) {
-  float raw;
+int readVBAT(void) {
+  int raw;
 
   // Set the analog reference to 3.0V (default = 3.6V)
   analogReference(AR_INTERNAL_3_0);
@@ -22,23 +22,71 @@ float readVBAT(void) {
   analogReference(AR_DEFAULT);
   analogReadResolution(10);
 
-  // Convert the raw value to mv
-  // ADC range is 0..3000mV and resolution is 12-bit (0..4095)
-  // VBAT voltage divider is 2M + 0.806M, which needs to be added back
-  return raw * VBAT_MV_PER_LSB * VBAT_DIVIDER_COMP;
+  return raw;
+}
+
+uint8_t mvToPercent(float mvolts) {
+    uint8_t battery_level;
+
+    if (mvolts >= 3000)
+    {
+        battery_level = 100;
+    }
+    else if (mvolts > 2900)
+    {
+        battery_level = 100 - ((3000 - mvolts) * 58) / 100;
+    }
+    else if (mvolts > 2740)
+    {
+        battery_level = 42 - ((2900 - mvolts) * 24) / 160;
+    }
+    else if (mvolts > 2440)
+    {
+        battery_level = 18 - ((2740 - mvolts) * 12) / 300;
+    }
+    else if (mvolts > 2100)
+    {
+        battery_level = 6 - ((2440 - mvolts) * 6) / 340;
+    }
+    else
+    {
+        battery_level = 0;
+    }
+
+    return battery_level;
 }
 
 void setup() {
   Serial.begin(115200);
+
+  // Get a single ADC sample and throw it away
+  readVBAT();
 }
 
 void loop() {
-  // Get a fresh ADC value
-  float vbat_val = readVBAT();
+  // Get a raw ADC reading
+  int vbat_raw = readVBAT();
+
+  // Convert from raw mv to percentage (based on LIPO chemistry)
+  uint8_t vbat_per = mvToPercent(vbat_raw * VBAT_MV_PER_LSB);
+
+  // Convert the raw value to compensated mv, taking the resistor-
+  // divider into account (providing the actual LIPO voltage)
+  // ADC range is 0..3000mV and resolution is 12-bit (0..4095),
+  // VBAT voltage divider is 2M + 0.806M, which needs to be added back
+  float vbat_mv = (float)vbat_raw * VBAT_MV_PER_LSB * VBAT_DIVIDER_COMP;
 
   // Display the results
-  Serial.print(vbat_val);
-  Serial.println(" mV");
+  Serial.print("ADC = ");
+  Serial.print(vbat_raw * VBAT_MV_PER_LSB);
+  Serial.print(" mV (");
+  Serial.print(vbat_raw);
+  Serial.print(") ");
+  Serial.print("LIPO = ");
+  Serial.print(vbat_mv);
+  Serial.print(" mV (");
+  Serial.print(vbat_per);
+  Serial.println("%)");
 
   delay(1000);
 }
