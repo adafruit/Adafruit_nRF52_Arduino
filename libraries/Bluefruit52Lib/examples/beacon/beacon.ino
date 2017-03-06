c29e3f8826ef9ac875e94c09c0667c6bb1e218db
Kevin Townsend <microbuilder@users.noreply.github.com>
2017-03-06 14:48:05 +0100

    General cleanup

--- libraries/Bluefruit52Lib/examples/beacon/beacon.ino
+++ libraries/Bluefruit52Lib/examples/beacon/beacon.ino
@@ -13,8 +13,11 @@
 *********************************************************************/
 #include <bluefruit.h>
 
+// Beacon uses the Manufacturer Specific Data field in the advertising
+// packet, which means you must provide a valid Manufacturer ID. Update
+// the field below to an appropriate value. For a list of valid IDs see:
 // https://www.bluetooth.com/specifications/assigned-numbers/company-identifiers
-// 0x004C is Apple
+// 0x004C is Apple (for example)
 #define MANUFACTURER_ID   0x004C 
 
 // AirLocate UUID: E2C56DB5-DFFB-48D2-B060-D0F5A71096E0
@@ -24,7 +27,8 @@ uint8_t beaconUuid[16] =
   0xB0, 0x60, 0xD0, 0xF5, 0xA7, 0x10, 0x96, 0xE0, 
 };
 
-// uuid, major, minor, rssi @1m
+// A valid Beacon packet consists of the following information:
+// UUID, Major, Minor, RSSI @ 1M
 BLEBeacon beacon(beaconUuid, 0x0001, 0x0000, -54);
 
 void setup() 
@@ -36,29 +40,32 @@ void setup()
   Bluefruit.begin();
   Bluefruit.setName("Bluefruit52");
 
+  // Manufacturer ID is required for Manufacturer Specific Data
   beacon.setManufacturer(MANUFACTURER_ID);
 
-  // Set up Advertising Packet
+  // Setup the advertising packet
   setupAdv();
 
-  // Start Advertising
+  // Start advertising
   Bluefruit.Advertising.start();
 }
 
 void setupAdv(void)
 {  
+  // Set the beacon payload using the BLEBeacon class populated
+  // earlier in this example
   Bluefruit.Advertising.setBeacon(beacon);
 
-  char* adv = Bluefruit.Advertising.getData();
+  // char* adv = Bluefruit.Advertising.getData();
 
-  // There is no room for Name in Advertising packet
-  // Use Scan response for Name
+  // There is no room left for 'Name' in the advertising packet
+  // Use the optinal secondary Scan Response packet for 'Name' instead
   Bluefruit.ScanResponse.addName();
 }
 
 void loop() 
 {
-  // Toggle both LEDs every 1 second
+  // Toggle both LEDs every second
   digitalToggle(LED_BUILTIN);
   delay(1000);
 }
