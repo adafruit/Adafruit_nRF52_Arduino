e191a07decc72c9b03ba92ee2ead4ed05e5db45e
hathach <thach@tinyusb.org>
2017-04-10 17:28:21 +0700

    forgot to add the file

---
+++ cores/nRF5/utility/SoftwareTimer.h
@@ -0,0 +1,67 @@
+/**************************************************************************/
+/*!
+    @file     SoftwareTimer.h
+    @author   hathach
+
+    @section LICENSE
+
+    Software License Agreement (BSD License)
+
+    Copyright (c) 2017, Adafruit Industries (adafruit.com)
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+    1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holders nor the
+    names of its contributors may be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
+    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+/**************************************************************************/
+#ifndef SOFTWARETIMER_H_
+#define SOFTWARETIMER_H_
+
+#include "Arduino.h"
+
+class SoftwareTimer
+{
+  private:
+    TimerHandle_t _handle;
+
+  public:
+    SoftwareTimer() { _handle = NULL; }
+
+    void begin(uint32_t ms, TimerCallbackFunction_t callback)
+    {
+      _handle = xTimerCreate(NULL, ms2tick(ms), true, NULL, callback);
+    }
+
+    void start(void) { xTimerStart(_handle, 0); }
+    void stop (void)  { xTimerStop(_handle, 0); }
+
+    void setPeriod(uint32_t ms)
+    {
+      BaseType_t active = xTimerIsTimerActive(_handle);
+      xTimerChangePeriod(_handle, ms2tick(ms), 0);
+
+      // Change period of inactive timer will also start it !!
+      if ( !active ) xTimerStop(_handle, 0);
+    }
+};
+
+#endif /* SOFTWARETIMER_H_ */
