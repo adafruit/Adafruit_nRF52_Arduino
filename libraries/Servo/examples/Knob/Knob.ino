1ede1a627608c3b82970241d981c647768e25379
hathach <thach@tinyusb.org>
2017-07-25 16:49:42 +0700

    change servo example pin, #68

--- libraries/Servo/examples/Knob/Knob.ino
+++ libraries/Servo/examples/Knob/Knob.ino
@@ -11,11 +11,11 @@
 
 Servo myservo;  // create servo object to control a servo
 
-int potpin = 0;  // analog pin used to connect the potentiometer
+int potpin = A0;  // analog pin used to connect the potentiometer
 int val;    // variable to read the value from the analog pin
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(A1);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
--- libraries/Servo/examples/Sweep/Sweep.ino
+++ libraries/Servo/examples/Sweep/Sweep.ino
@@ -15,7 +15,7 @@ Servo myservo;  // create servo object to control a servo
 int pos = 0;    // variable to store the servo position
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(A0);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
