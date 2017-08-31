b80c5a89bf54798809d39d9a692654c03516e5e2
hathach <thach@tinyusb.org>
2017-08-31 18:15:38 +0700

    test systemOff without freeRTOS

--- libraries/Bluefruit52Lib/examples/Hardware/blinky/blinky.ino
+++ libraries/Bluefruit52Lib/examples/Hardware/blinky/blinky.ino
@@ -20,13 +20,17 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
 }
-
+     
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+#if 0  
+  for (int i = 0; i < 2; i++)
+  {
+    digitalToggle(LED_BLUE);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+  }
+#endif
+
+  systemOff(A0, HIGH);
 }
