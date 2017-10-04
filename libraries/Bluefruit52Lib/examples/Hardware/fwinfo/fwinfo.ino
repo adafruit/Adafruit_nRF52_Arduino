8372de1592ae5d1d55167210e208b305b4683079
hathach <thach@tinyusb.org>
2017-10-04 17:02:54 +0700

    update programmer tool to migrate bootloader easily
    - add fwinfo sketch

---
+++ libraries/Bluefruit52Lib/examples/Hardware/fwinfo/fwinfo.ino
@@ -0,0 +1,31 @@
+/*********************************************************************
+ This is an example for our nRF52 based Bluefruit LE modules
+
+ Pick one up today in the adafruit shop!
+
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
+
+ MIT license, check LICENSE for more information
+ All text above, and the splash screen below must be included in
+ any redistribution
+*********************************************************************/
+
+#include <Arduino.h>
+
+
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println("Bluefruit52 Firmware Info Example");
+  Serial.println("---------------------------------\n");
+}
+
+
+void loop() 
+{
+  dbgPrintVersion();
+  digitalToggle(LED_RED);
+  delay(5000);                       // wait for a second
+}
