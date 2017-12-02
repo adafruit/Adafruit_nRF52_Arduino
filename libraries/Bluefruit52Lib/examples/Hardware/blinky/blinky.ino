a166da3b5990d1f3896dc45292b16b279e6f9ce5
Nick Pisarro <infinityminusnine@gmail.com>
2017-12-02 12:24:01 -0800

    Fix & upgrade blink sketch

--- libraries/Bluefruit52Lib/examples/Hardware/blinky/blinky.ino
+++ libraries/Bluefruit52Lib/examples/Hardware/blinky/blinky.ino
@@ -2,35 +2,26 @@
   Blink
   Turns on an LED on for one second, then off for one second, repeatedly.
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care 
-  of use the correct LED pin whatever is the board used.
-  If you want to know what pin the on-board LED is connected to on your Arduino model, check
-  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
-  
   This example code is in the public domain.
+ */
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-*/
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // initialize the digital pin as an output.
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
 }
-     
-// the loop function runs over and over again forever
-void loop() {
-#if 0  
-  for (int i = 0; i < 2; i++)
-  {
-    digitalToggle(LED_BLUE);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);                       // wait for a second
-  }
-#endif
 
-  systemOff(A0, HIGH);
+// the loop routine runs over and over again forever
+void loop() {
+  digitalWrite(LED_BLUE, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);               // wait for a second
+  digitalWrite(LED_BLUE, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second
+  digitalWrite(LED_RED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);               // wait for a second
+  digitalWrite(LED_RED, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second
 }
