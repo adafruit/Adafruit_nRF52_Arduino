a6fa818cfff56e59007d10de1d4c72ffcf0fc977
Jeremy Poulter <jeremy@bigjungle.net>
2018-05-05 21:18:14 +0100

    Updated as per comments

--- libraries/Bluefruit52Lib/src/bluefruit.cpp
+++ libraries/Bluefruit52Lib/src/bluefruit.cpp
@@ -235,12 +235,12 @@ err_t AdafruitBluefruit::begin(uint8_t prph_count, uint8_t central_count)
   {
       // LXRC
       .source        = NRF_CLOCK_LF_SRC_RC,
-      .rc_ctiv       = 8, //16
+      .rc_ctiv       = 16,
       .rc_temp_ctiv  = 2,
       #if SD_VER < 500
       .xtal_accuracy = NRF_CLOCK_LF_XTAL_ACCURACY_250_PPM
       #else
-      .accuracy      = NRF_CLOCK_LF_ACCURACY_20_PPM
+      .accuracy      = NRF_CLOCK_LF_ACCURACY_250_PPM
       #endif
   };
 #else
