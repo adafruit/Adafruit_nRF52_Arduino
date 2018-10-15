4950de9ea38d4a66c217dc4eee20418e46c34dba
hathach <thach@tinyusb.org>
2018-10-15 15:18:34 +0700

    move custom start up to linker folder

--- libraries/FileSystem/src/nffs_port.c
+++ libraries/FileSystem/src/nffs_port.c
@@ -1,6 +1,6 @@
 /**************************************************************************/
 /*!
-    @file     nrf52_flash.h
+    @file     nffs_port.c
     @author   hathach (tinyusb.org)
 
     @section LICENSE
@@ -33,28 +33,29 @@
     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 /**************************************************************************/
-#ifndef NRF52_FLASH_H_
-#define NRF52_FLASH_H_
 
-#include <stdint.h>
-#include <hal/hal_flash_int.h>
+#include "kernel.h"
+#include "nffs/nffs.h"
 
-#ifdef __cplusplus
- extern "C" {
-#endif
+nffs_os_mempool_t nffs_file_pool = sizeof(struct nffs_file);
+nffs_os_mempool_t nffs_dir_pool = sizeof(struct nffs_dir);
+nffs_os_mempool_t nffs_inode_entry_pool = sizeof(struct nffs_inode_entry);
+nffs_os_mempool_t nffs_block_entry_pool = sizeof(struct nffs_hash_entry);
+nffs_os_mempool_t nffs_cache_inode_pool = sizeof(struct nffs_cache_inode);
+nffs_os_mempool_t nffs_cache_block_pool = sizeof(struct nffs_cache_block);
 
-#define NRF52K_FLASH_SECTOR_SZ	4096
+int nffs_os_mempool_init(void)
+{
+  return 0;
+}
 
-// const struct hal_flash*  dev is required for nffs flash interface but not used
-// by nrf52_flash, just simply pass NULL when invoked
+void* nffs_os_mempool_get(nffs_os_mempool_t *pool)
+{
+  return pvPortMalloc(*pool);
+}
 
-int nrf52_flash_init(const struct hal_flash *dev);
-int nrf52_flash_erase_sector(const struct hal_flash *dev, uint32_t sector_address);
-int nrf52_flash_write(const struct hal_flash *dev, uint32_t address, const void *src, uint32_t num_bytes);
-int nrf52k_flash_read(const struct hal_flash *dev, uint32_t address, void *dst, uint32_t num_bytes);
-
-#ifdef __cplusplus
- }
-#endif
-
-#endif /* NRF52_FLASH_H_ */
+int nffs_os_mempool_free(nffs_os_mempool_t *pool, void *block)
+{
+  vPortFree(block);
+  return 0;
+}
