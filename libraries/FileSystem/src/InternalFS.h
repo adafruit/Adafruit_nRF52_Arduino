03b500d078cef31a6baf085ea0291a54dc92a8fc
Jeremy Poulter <jeremy@bigjungle.net>
2019-03-13 21:37:58 +0000

    Restructured so the class can be extended to add support for external flash chips.

--- libraries/FileSystem/src/InternalFS.h
+++ libraries/FileSystem/src/InternalFS.h
@@ -58,6 +58,12 @@ class LittleFS: public BluefruitFS::FileSystemClass
     bool rmdir_r (char const *filepath);
     bool format (bool eraseall);
 
+  protected:
+    bool _begun;
+    bool _mounted;
+
+    LittleFS (lfs_size_t read_size, lfs_size_t prog_size, lfs_size_t block_size, lfs_size_t block_count, lfs_size_t lookahead);
+    
     // Internal API: shouldn't be used by Arduino sketch
     virtual size_t _f_write (void* fhdl, uint8_t const *buf, size_t size);
     virtual int _f_read (void* fhdl, void *buf, uint16_t nbyte);
@@ -67,17 +73,26 @@ class LittleFS: public BluefruitFS::FileSystemClass
     virtual uint32_t _f_position (void* fhdl);
     virtual uint32_t _f_size (void* fhdl);
 
+    // Raw flash opperations, override to use an external flash chip
+    virtual int _flash_read (lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size);
+    virtual int _flash_prog (lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size);
+    virtual int _flash_erase (lfs_block_t block);
+    virtual void _flash_erase_all();
+    virtual int _flash_sync ();
+
     virtual BluefruitFS::File _f_openNextFile (void* fhdl, char const* cwd, uint8_t mode);
     virtual void _f_rewindDirectory (void* fhdl);
-
   private:
     struct lfs_config _lfs_cfg;
     lfs_t _lfs;
-    bool _begun;
-    bool _mounted;
 
     BluefruitFS::File _open_file (char const *filepath, uint8_t mode);
     BluefruitFS::File _open_dir (char const *filepath);
+
+    static int _iflash_read (const struct lfs_config *c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size);
+    static int _iflash_prog (const struct lfs_config *c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size);
+    static int _iflash_erase (const struct lfs_config *c, lfs_block_t block);
+    static int _iflash_sync (const struct lfs_config *c);
 };
 
 extern LittleFS InternalFS;
