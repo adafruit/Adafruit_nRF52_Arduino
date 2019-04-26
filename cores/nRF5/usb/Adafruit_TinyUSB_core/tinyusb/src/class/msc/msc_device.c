1377101f310556752fcb9798872e3a82eabf16b6
hathach <thach@tinyusb.org>
2019-04-26 22:46:33 +0700

    update tinyusb

--- cores/nRF5/usb/Adafruit_TinyUSB_core/tinyusb/src/class/msc/msc_device.c
+++ cores/nRF5/usb/Adafruit_TinyUSB_core/tinyusb/src/class/msc/msc_device.c
@@ -163,8 +163,13 @@ bool mscd_control_request(uint8_t rhport, tusb_control_request_t const * p_reque
 
     case MSC_REQ_GET_MAX_LUN:
     {
-      // returned MAX LUN is minus 1 by specs
-      uint8_t maxlun = CFG_TUD_MSC_MAXLUN-1;
+      uint8_t maxlun = 1;
+      if (tud_msc_maxlun_cb) maxlun = tud_msc_maxlun_cb();
+      TU_VERIFY(maxlun);
+
+      // MAX LUN is minus 1 by specs
+      maxlun--;
+
       usbd_control_xfer(rhport, p_request, &maxlun, 1);
     }
     break;
