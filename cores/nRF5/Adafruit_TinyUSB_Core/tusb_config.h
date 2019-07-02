3afe3c52ba963b1f11e23e2cd4a6c52f172d15f3
hathach <thach@tinyusb.org>
2019-07-03 01:24:54 +0700

    enable tinyusb midi

--- cores/nRF5/Adafruit_TinyUSB_Core/tusb_config.h
+++ cores/nRF5/Adafruit_TinyUSB_Core/tusb_config.h
@@ -55,25 +55,22 @@
 #define CFG_TUD_MSC                 1
 #define CFG_TUD_HID                 1
 
-#define CFG_TUD_MIDI                0
+#define CFG_TUD_MIDI                1
 #define CFG_TUD_CUSTOM_CLASS        0
 
-//------------- CDC -------------//
-
-// FIFO size of CDC TX and RX
+// CDC FIFO size of TX and RX
 #define CFG_TUD_CDC_RX_BUFSIZE      256
 #define CFG_TUD_CDC_TX_BUFSIZE      256
 
-//------------- MSC -------------//
-
-// Buffer size of Device Mass storage
+// MSC Buffer size of Device Mass storage
 #define CFG_TUD_MSC_BUFSIZE         512
 
-//------------- HID -------------//
-
-// Should be sufficient to hold ID (if any) + Data
+// HID buffer size Should be sufficient to hold ID (if any) + Data
 #define CFG_TUD_HID_BUFSIZE         64
 
+// MIDI FIFO size of TX and RX
+#define CFG_TUD_MIDI_RX_BUFSIZE     64
+#define CFG_TUD_MIDI_TX_BUFSIZE     64
 
 #ifdef __cplusplus
  }
