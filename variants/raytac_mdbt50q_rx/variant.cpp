4d795916afbc9c323b60e86061b3c83a1dc7b916
hathach <thach@tinyusb.org>
2020-01-13 17:09:33 +0700

    rename mdbt50q board and variant to be consistent

--- variants/raytac_mdbt50q_rx/variant.cpp
+++ variants/raytac_mdbt50q_rx/variant.cpp
@@ -18,7 +18,8 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "variant.h"
+#include "../raytac_mdbt50q_rx/variant.h"
+
 #include "wiring_constants.h"
 #include "wiring_digital.h"
 #include "nrf.h"
