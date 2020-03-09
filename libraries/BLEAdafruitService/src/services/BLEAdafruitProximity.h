c7ace597fdca0cf5ee398944e55c7742b0895861
hathach <thach@tinyusb.org>
2020-03-09 18:28:05 +0700

    adding Proximity sensor

--- libraries/BLEAdafruitService/src/services/BLEAdafruitProximity.h
+++ libraries/BLEAdafruitService/src/services/BLEAdafruitProximity.h
@@ -22,27 +22,18 @@
  * THE SOFTWARE.
  */
 
-#ifndef BLEADAFRUITSERVICE_H_
-#define BLEADAFRUITSERVICE_H_
+#ifndef BLEADAFRUIT_PROXIMITY_H_
+#define BLEADAFRUIT_PROXIMITY_H_
 
-#include "bluefruit_common.h"
-#include "BLECharacteristic.h"
-#include "BLEService.h"
-#include "services/BLEAdafruitSensor.h"
+class BLEAdafruitProximity : public BLEAdafruitSensor
+{
+  public:
+    static const uint8_t UUID128_SERVICE[16];
+    static const uint8_t UUID128_CHR_DATA[16];
 
-#include "services/BLEAdafruitAccel.h"
-#include "services/BLEAdafruitAddressablePixel.h"
-#include "services/BLEAdafruitBaro.h"
-#include "services/BLEAdafruitButton.h"
-#include "services/BLEAdafruitColor.h"
-#include "services/BLEAdafruitGyro.h"
-#include "services/BLEAdafruitHumid.h"
-#include "services/BLEAdafruitLightSensor.h"
-#include "services/BLEAdafruitMagnetic.h"
-#include "services/BLEAdafruitProximity.h"
-#include "services/BLEAdafruitQuaternion.h"
-#include "services/BLEAdafruitSound.h"
-#include "services/BLEAdafruitTemperature.h"
-#include "services/BLEAdafruitTone.h"
+    BLEAdafruitProximity(void);
 
-#endif /* BLEADAFRUITSERVICE_H_ */
+    using BLEAdafruitSensor::begin;
+};
+
+#endif /* BLEADAFRUIT_PROXIMITY_H_ */
