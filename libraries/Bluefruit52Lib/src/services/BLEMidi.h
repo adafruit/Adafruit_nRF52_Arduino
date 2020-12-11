275a8a4f9dbd966b67f9ef9043f5b328f9e90073
Nathan Sollenberger <nsollenberger@gmail.com>
2020-12-11 16:47:37 -0700

    Adds missing Transport methods

--- libraries/Bluefruit52Lib/src/services/BLEMidi.h
+++ libraries/Bluefruit52Lib/src/services/BLEMidi.h
@@ -35,6 +35,8 @@
 #ifndef BLEMIDI_H_
 #define BLEMIDI_H_
 
+#include <MIDI.h>
+
 #include "bluefruit_common.h"
 #include "utility/adafruit_fifo.h"
 
@@ -73,6 +75,9 @@ class BLEMidi: public BLEService, public Stream
 
     void setWriteCallback(midi_write_cb_t fp);
     void autoMIDIread(void* midi_obj);
+
+    bool beginTransmission(MIDI_NAMESPACE::MidiType type);
+    void endTransmission();
 
     // Stream API for MIDI Interface
     virtual int    read       ( void );
