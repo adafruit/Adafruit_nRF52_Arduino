d65a8686ce908086f6e16d337c8fdbfa413e497b
Erich Bieri <erich@bieris.net>
2022-06-17 23:03:12 +0200

    Added argon variants

---
+++ variants/particle_argon/variant.cpp
@@ -0,0 +1,100 @@
+/*
+  Copyright (c) 2014-2015 Arduino LLC.  All right reserved.
+  Copyright (c) 2016 Sandeep Mistry All right reserved.
+  Copyright (c) 2018, Adafruit Industries (adafruit.com)
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "variant.h"
+#include "wiring_constants.h"
+#include "wiring_digital.h"
+#include "nrf.h"
+#include "nrf_pwm.h"
+
+const uint32_t g_ADigitalPinMap[] =
+{
+  // D0 .. D8
+  _PINNUM(0, 26),   // P0.26 (SDA)
+  _PINNUM(0, 27),   // P0.27 (SCL)
+  _PINNUM(1, 1),    // P1.01 (SDA1) (UART1_RTS) (SPI1_SCK) (PWM3)
+  _PINNUM(1, 2),    // P1.02 (SCL1) (UART1_CTS) (SPI1_MOSI) (PWM3)
+  _PINNUM(1, 5),    // P1.05 (UART2_TX) (SPI1_MISO) (PWM1) D4
+  _PINNUM(1, 4),    // P1.04 (UART2_RX) (PWM1) D5
+  _PINNUM(1, 6),    // P1.06 (UART2_CTS) (PWM1) D6
+  _PINNUM(1, 12),   // P1.12 (PWM0) D7
+  _PINNUM(1, 7),    // P1.07 (UART2_RTS) D8
+
+  // D9 .. D21
+  _PINNUM(0, 6),    // P0.06 (UART1_TX)
+  _PINNUM(0, 8),    // P0.08 (UART1_RX)
+  _PINNUM(1, 14),   // P1.14 (SPI_MISO)
+  _PINNUM(1, 13),   // P1.13 (SPI_MOSI)
+  _PINNUM(1, 15),   // P1.15 (SPI_SCK)
+  _PINNUM(0, 31),   // P0.31 (PWM3) (SPI_SS) (ADC5)
+  _PINNUM(0, 30),   // P0.30 (PWM3) (ADC4)
+  _PINNUM(0, 29),   // P0.29 (PWM2) (ADC3)
+  _PINNUM(0, 28),   // P0.28 (PWM2) (ADC2)
+  _PINNUM(0, 4),    // P0.04 (PWM2) (ADC1)
+  _PINNUM(0, 3),    // P0.03 (PWM2) (ADC0)
+  _PINNUM(0, 11),   // P0.11 (MODE BUTTON)
+  _PINNUM(0, 18),   // P0.18 (RESET)
+
+  // D22 .. D24
+  // LEDS
+  _PINNUM(0, 13),   // P0.13 (RGB_RED)
+  _PINNUM(0, 14),   // P0.14 (RGB_GREEN)
+  _PINNUM(0, 15),   // P0.15 (RGB_BLUE) 
+
+  // D25 .. D26
+  // Antenna
+  _PINNUM(0, 2),    // P0.02 (ANTENNA_SWITCH_1 - PCB ANTENNA)
+  _PINNUM(0, 25),   // P0.25 (ANTENNA_SWITCH_2 - EXTERNAL u.FL)
+
+  // D27 .. D28
+  // NFC
+  _PINNUM(0, 9),    // P0.09 (u.FL FOR NFC ANTENNA)
+  _PINNUM(0, 10),   // P0.10 (u.FL FOR NFC ANTENNA)
+
+  // D29 .. 36
+  // Analog Pins A0 .. A7
+  _PINNUM(0, 3),    // P0.03 (A0)
+  _PINNUM(0, 4),    // P0.04 (A1)
+  _PINNUM(0, 28),   // P0.28 (A2)
+  _PINNUM(0, 29),   // P0.29 (A3)
+  _PINNUM(0, 30),   // P0.30 (A4)
+  _PINNUM(0, 31),   // P0.31 (A5)
+  _PINNUM(0, 5),    // P0.05 (BAT_DET/VBAT)
+  _PINNUM(0, 2),    // P0.02 (AREF)
+
+  // D37 .. D38
+  // Power status
+  _PINNUM(0, 12),  // P0.12 (PWR)
+  _PINNUM(1, 9),   // P1.09 (CHG)
+
+  // D39 .. D41
+  // ESP32
+  _PINNUM(0, 16),  // P0.16 (BOOT_MODE)
+  _PINNUM(0, 24),  // P0.24 (WIFI_EN)
+  _PINNUM(0, 7),   // P0.07 (HOST_WAKE)
+
+};
+
+void initVariant()
+{
+  // LED1
+  pinMode(PIN_LED1, OUTPUT);
+  ledOff(PIN_LED1);
+}
---
+++ variants/particle_argon/variant.h
@@ -0,0 +1,223 @@
+ /*
+  Copyright (c) 2014-2015 Arduino LLC.  All right reserved.
+  Copyright (c) 2016 Sandeep Mistry All right reserved.
+  Copyright (c) 2018, Adafruit Industries (adafruit.com)
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#ifndef _VARIANT_PARTICLE_ARGON_
+#define _VARIANT_PARTICLE_ARGON_
+
+#define _PINNUM(port, pin)    ((port)*32 + (pin))
+
+/** Master clock frequency */
+#define VARIANT_MCK       (64000000ul)
+
+#define USE_LFXO      // Board uses 32khz crystal for LF
+// define USE_LFRC    // Board uses RC for LF
+
+/*----------------------------------------------------------------------------
+ *        Headers
+ *----------------------------------------------------------------------------*/
+
+#include "WVariant.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
+// Number of pins defined in PinDescription array
+#define PINS_COUNT           (41)
+#define NUM_DIGITAL_PINS     (41)
+#define NUM_ANALOG_INPUTS    (6) // A6 is used for battery, A7 is analog reference
+#define NUM_ANALOG_OUTPUTS   (0)
+
+/*
+ * Digital pins
+ */
+#define PIN_D0              (0)
+#define PIN_D1              (1)
+#define PIN_D2              (2)
+#define PIN_D3              (3)
+#define PIN_D4              (4)
+#define PIN_D5              (5)
+#define PIN_D6              (6)
+#define PIN_D7              (7)
+#define PIN_D8              (8)
+#define PIN_D9              (9)
+#define PIN_D10             (10)
+#define PIN_D11             (11)
+#define PIN_D12             (12)
+#define PIN_D13             (13)
+#define PIN_D14             (14)
+#define PIN_D15             (15)
+#define PIN_D16             (16)
+#define PIN_D17             (17)
+#define PIN_D18             (18)
+#define PIN_D19             (19)
+#define PIN_D20             (20)
+#define PIN_D21             (21)
+
+static const uint8_t D0  = PIN_D0 ;
+static const uint8_t D1  = PIN_D1 ;
+static const uint8_t D2  = PIN_D2 ;
+static const uint8_t D3  = PIN_D3 ;
+static const uint8_t D4  = PIN_D4 ;
+static const uint8_t D5  = PIN_D5 ;
+static const uint8_t D6  = PIN_D6 ;
+static const uint8_t D7  = PIN_D7 ;
+static const uint8_t D8  = PIN_D8 ;
+static const uint8_t D9  = PIN_D9 ;
+static const uint8_t D10 = PIN_D10;
+static const uint8_t D11 = PIN_D11;
+static const uint8_t D12 = PIN_D12;
+static const uint8_t D13 = PIN_D13;
+static const uint8_t D14 = PIN_D14;
+static const uint8_t D15 = PIN_D15;
+static const uint8_t D16 = PIN_D16;
+static const uint8_t D17 = PIN_D17;
+static const uint8_t D18 = PIN_D18;
+static const uint8_t D19 = PIN_D19;
+static const uint8_t D20 = PIN_D20;
+
+// LEDs
+#define LEDS_NUMBER    1
+#define LED_STATE_ON   1
+
+#define BOARD_RGB_BRIGHTNESS 0x202020
+
+#define PIN_LED1             (7)
+#define LED_BUILTIN          PIN_LED1
+#define LED_CONN             PIN_LED1
+
+#define LED_RED              PIN_LED1
+#define LED_BLUE             PIN_LED1
+
+static const uint8_t LED_RGB_RED   = (22);
+static const uint8_t LED_RGB_GREEN = (23);
+static const uint8_t LED_RGB_BLUE  = (24);
+
+// Buttons
+#define BUTTONS_NUMBER 2
+#define BUTTON_DFU                  (20)
+#define BUTTON_FRESET               (19) // A0
+#define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
+
+static const uint8_t BUTTON_MODE =  BUTTON_DFU;
+
+// Antenna
+#define ANTENNA_SWITCH_1            (25)
+#define ANTENNA_SWITCH_2            (26)
+
+// NFC
+#define NFC1                        (27)
+#define NFC2                        (28)
+
+/*
+ * Analog pins
+ */
+#define PIN_A0                      (29)
+#define PIN_A1                      (30)
+#define PIN_A2                      (31)
+#define PIN_A3                      (32)
+#define PIN_A4                      (33)
+#define PIN_A5                      (34)
+#define PIN_A6                      (35)
+#define PIN_A7                      (36)
+
+static const uint8_t A0 = PIN_A0;
+static const uint8_t A1 = PIN_A1;
+static const uint8_t A2 = PIN_A2;
+static const uint8_t A3 = PIN_A3;
+static const uint8_t A4 = PIN_A4;
+static const uint8_t A5 = PIN_A5;
+static const uint8_t A6 = PIN_A6;
+static const uint8_t A7 = PIN_A7;
+#define ADC_RESOLUTION    14
+
+
+// Other pins
+#define PIN_AREF           PIN_A7
+#define PIN_VBAT           PIN_A6
+#define PIN_PWR            (37)
+#define PIN_CHG            (38)
+
+#define PIN_BOOT_MODE      (39)
+#define PIN_WIFI_EN        (40)
+#define PIN_HOST_WAKE      (41)
+
+static const uint8_t AREF = PIN_AREF;
+
+/*
+ * Serial interfaces
+ */
+#define PIN_SERIAL1_TX         9
+#define PIN_SERIAL1_RX         10
+#define PIN_SERIAL1_CTS        4
+#define PIN_SERIAL1_RTS        3
+
+// ESP32 Network Co-Processor
+#define PIN_SERIAL_ESP32_RX    5
+#define PIN_SERIAL_ESP32_TX    4
+#define PIN_SERIAL_ESP32_CTS   6
+#define PIN_SERIAL_ESP32_RTS   8
+
+/*
+ * SPI Interfaces
+ */
+#define SPI_INTERFACES_COUNT 2
+
+#define PIN_SPI_SS                  14
+#define PIN_SPI_MISO                11
+#define PIN_SPI_MOSI                12
+#define PIN_SPI_SCK                 13
+#define PIN_SPI1_MISO               4
+#define PIN_SPI1_MOSI               3
+#define PIN_SPI1_SCK                2
+
+static const uint8_t SS   = PIN_SPI_SS;
+static const uint8_t MOSI = PIN_SPI_MOSI;
+static const uint8_t MISO = PIN_SPI_MISO;
+static const uint8_t SCK  = PIN_SPI_SCK;
+static const uint8_t MOSI1 = PIN_SPI1_MOSI;
+static const uint8_t MISO1 = PIN_SPI1_MISO;
+static const uint8_t SCK1  = PIN_SPI1_SCK;
+
+/*
+ * Wire Interfaces
+ */
+#define WIRE_INTERFACES_COUNT 2
+
+#define PIN_WIRE_SDA               0
+#define PIN_WIRE_SCL               1
+#define PIN_WIRE1_SDA              2
+#define PIN_WIRE1_SCL              3
+
+// On-board QSPI Flash
+#define EXTERNAL_FLASH_DEVICES   MX25L3233F
+
+#define USB_MSC_BLOCK_SIZE    512
+#define USB_MSC_BLOCK_COUNT   ((2*1024*1024) / USB_MSC_BLOCK_SIZE)
+
+#define EXTERNAL_FLASH_USE_QSPI
+
+#ifdef __cplusplus
+}
+#endif
+
+/*----------------------------------------------------------------------------
+ *        Arduino objects - C++ only
+ *----------------------------------------------------------------------------*/
+
+#endif
