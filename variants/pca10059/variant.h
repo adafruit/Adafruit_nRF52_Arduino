656c1c6178b704dad6cb0354bf7cc54ab2eb2b35
bobxor <robert.mcmanus@gmail.com>
2022-12-22 09:00:08 -0800

    Update variant.h
    Additional deprecated code lines removed.

--- variants/pca10059/variant.h
+++ variants/pca10059/variant.h
@@ -86,13 +86,6 @@ static const uint8_t A6  = PIN_A6 ;
 static const uint8_t A7  = PIN_A7 ;
 #define ADC_RESOLUTION    14
 
-// Other pins
-// #define PIN_AREF           (2)
-// #define PIN_NFC1           (9)
-// #define PIN_NFC2           (10)
-
-// static const uint8_t AREF = PIN_AREF;
-
 /*
  * Serial interfaces
  */
