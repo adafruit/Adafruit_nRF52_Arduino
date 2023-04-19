97649cee90d8322703e64f5b13a455899a726e72
Charlie B <11893617+qubitter@users.noreply.github.com>
2023-04-19 17:16:49 -0400

    added client IAS

--- libraries/Bluefruit52Lib/src/clients/BLEClientIas.cpp
+++ libraries/Bluefruit52Lib/src/clients/BLEClientIas.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "BLEClientIas.h"
+#include "bluefruit.h"
+
+BLEClientIas::BLEClientIas() : BLEClientService(UUID16_SVC_IMMEDIATE_ALERT) {
+
+}
+
+bool BLEClientIas::begin(void) {
+    // invoke superclass begin()
+    BLEClientService::begin();
+    return true;
+}
+
+bool BLEClientIas::discover(uint16_t conn_handle) {
+    VERIFY(BLEClientService::discover(conn_handle));
+
+    return true;
+}
+
+uint16_t BLEClientIas::getAlertLevel() {
+
+    uint16_t level = 0;
+
+    BLEClientCharacteristic chr(uuid);
+    chr.begin(this);
+
+    if (Bluefruit.Discovery.discoverCharacteristic(_conn_hdl, chr)) {
+        level = chr.read16();
+    }
+
+    Bluefruit.Discovery.setHandleRange(bck_range);
+
+    return level;
+}
--- libraries/Bluefruit52Lib/src/clients/BLEClientIas.h
+++ libraries/Bluefruit52Lib/src/clients/BLEClientIas.h
@@ -5,8 +5,19 @@
 #ifndef ADAFRUIT_NRF52_ARDUINO_BLECLIENTIAS_H
 #define ADAFRUIT_NRF52_ARDUINO_BLECLIENTIAS_H
 
+#include "bluefruit_common.h"
+#include "BLEClientCharacteristic.h"
+#include "BLEClientService.h"
 
-class BLEClientIas {
+class BLEClientIas : public BLEClientService {
+
+public:
+    BLEClientIas(void);
+
+    virtual bool begin(void);
+    virtual bool discover(uint16_t conn_handle);
+
+    uint16_t getAlertLevel (void);
 
 };
 
